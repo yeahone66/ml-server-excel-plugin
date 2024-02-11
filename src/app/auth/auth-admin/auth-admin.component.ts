891f995ffd42df34d54955e7c0e8fcd4ca3fbb2b
yeahone66 <69949080+yeahone66@users.noreply.github.com>
2024-02-11 19:34:07 +0000

    Cleaned out the intertubes

--- src/app/auth/auth-admin/auth-admin.component.ts
+++ src/app/auth/auth-admin/auth-admin.component.ts
@@ -1,1 +1,15 @@
-import { Component, OnInit } from '@angular/co
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
+// Component for local/LDAP/LDAP-S authentication
+@Component({
+  selector: 'app-auth-admin',
+  templateUrl: './auth-admin.component.html',
+  styleUrls: ['./auth-admin.component.css']
+})
+export class AuthAdminComponent implements OnInit {
+
+  pu
