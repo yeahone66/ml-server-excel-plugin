0ba7982f3c382f4c5e2c1a4f48323e4405e8aac2
yeahone66 <69949080+yeahone66@users.noreply.github.com>
2024-02-19 15:33:55 +0000

    version 1.0.8

--- src/app/auth/auth-azure/auth-azure.component.spec.ts
+++ src/app/auth/auth-azure/auth-azure.component.spec.ts
@@ -4,4 +4,15 @@ import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule } from '@angular/http';
-import { FormsModule 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Components
+import { AuthAzureComponent } from './auth-azure.component';
+
+// Services
+import { AuthService } from '../../services/auth.service';
+import { MlsService } from '../../services/mls.service';
+
+// Integration tests for AAD component
+describe('AuthAzureComponent', function
