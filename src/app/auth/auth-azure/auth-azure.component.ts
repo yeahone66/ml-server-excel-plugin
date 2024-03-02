97b8f8e1702f8927e0c290770039008d7f015983
yeahone66 <69949080+yeahone66@users.noreply.github.com>
2024-03-02 13:25:33 +0000

    2003-09-04\tJoel Sherrill <joel@OARcorp.com>

--- src/app/auth/auth-azure/auth-azure.component.ts
+++ src/app/auth/auth-azure/auth-azure.component.ts
@@ -1,3 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } f
+import { AuthService } from '../../services/auth.service';
+import { NgForm } from '@angular/forms';
+
+// Component for AAD authentication
+@Component({
+    selector: 'app-auth-azure',
+    templateUrl: './auth-azure.component.html',
+    styleUrls: ['./auth-azure.component.css']
+})
+export class AuthAzureComponent implements OnInit {
+
+    public config: {
+        connection: s
