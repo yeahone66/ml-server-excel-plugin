8cbcbba4b47806cd01d3e366d3d49266b0fb1d69
yeahone66 <69949080+yeahone66@users.noreply.github.com>
2024-03-10 14:48:13 +0000

    add travis build status to README

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,3 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
-import { Router } fr
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+import { MlsService } from '../services/mls.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor(public authService: AuthService, public mlsService: MlsService, private ro
