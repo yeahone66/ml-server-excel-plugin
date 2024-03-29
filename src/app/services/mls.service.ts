b7429f5af1de6f65c10d2b45d019b21a95fec9fb
yeahone66 <69949080+yeahone66@users.noreply.github.com>
2024-03-29 17:05:17 +0000

    libceph: tweak ceph_alloc_msg()

--- src/app/services/mls.service.ts
+++ src/app/services/mls.service.ts
@@ -4,4 +4,14 @@ import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 
 // Services
-import { AuthService
+import { AuthService } from './auth.service';
+
+// Models
+import { WebService, ExcelParameter } from '../models/web-service';
+
+// Service which makes API calls to GET services and POST to an individual service
+@Injectable()
+export class MlsService {
+
+    // Boolean field for WebService and WebServices component templates to know whether a service has been selected or not
+    public serviceSelected = f
