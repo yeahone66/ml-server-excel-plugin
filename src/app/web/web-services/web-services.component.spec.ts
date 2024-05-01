05b8c56bff2eb03207bd47fc7c67e434c70f6977
yeahone66 <69949080+yeahone66@users.noreply.github.com>
2024-05-01 16:28:32 +0000

    Update FloatFloats.jl

--- src/app/web/web-services/web-services.component.spec.ts
+++ src/app/web/web-services/web-services.component.spec.ts
@@ -1,2 +1,11 @@
 import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platfor
+import { By } from '@angular/platform-browser';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
+
+// Components
+import { WebServicesComponent } from '.
